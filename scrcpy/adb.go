4796ba6d56c1a03ea29566d7e5e1e578b333dbe4
llbec <30335141+bicliu@users.noreply.github.com>
2020-12-22 02:56:20 +0800

    update

--- scrcpy/adb.go
+++ scrcpy/adb.go
@@ -3,17 +3,82 @@ package scrcpy
 import (
 	"bytes"
 	"fmt"
-	"io/ioutil"
 	"log"
 	"os"
 	"os/exec"
+	"regexp"
 	"runtime"
 	"strings"
 	"sync"
 )
 
+func adbDevices() []string {
+	var list []string
+	ret, err := adbExecOut("", "devices")
+	if err != nil {
+		if debugOpt.Error() {
+			log.Printf("adb devices failed: %s(%s)", err.Error(), ret)
+			return list
+		}
+	}
+	//fmt.Println(ret)
+	if len(ret) != 0 {
+		reg, err := regexp.Compile("([\\S]+)[\\s]+device\\b")
+		if err == nil {
+			list = reg.FindAllString(ret, -1)
+			//fmt.Println(list)
+		}
+	}
+	for i, v := range list {
+		list[i] = strings.Fields(strings.TrimSpace(v))[0]
+	}
+	return list
+}
+
 func adbTCPMod(serial string, port int) error {
 	return adbExec(serial, "tcpip", fmt.Sprintf("%d", port))
+}
+
+func adbGetIP(serial string) string {
+	res, err := adbExecOut(serial, "shell", "ip", "-o", "a")
+	if err != nil {
+		if debugOpt.Error() {
+			log.Printf("adb shell ip failed: %s(%s)", err.Error(), res)
+			return ""
+		}
+	}
+	if len(res) > 0 {
+		reg, err := regexp.Compile("\\swlan0[\\s]+inet\\s([\\d]+.[\\d]+.[\\d]+.[\\d]+)")
+		if err == nil {
+			list := reg.FindAllString(res, -1)
+			if len(list) > 0 {
+				ips := strings.Fields(strings.TrimSpace(list[0]))
+				if len(ips) == 3 {
+					return ips[2]
+				}
+				if debugOpt.Error() {
+					log.Printf("adbGetIP regex invalid format: %d", len(ips))
+				}
+			} else {
+				if debugOpt.Error() {
+					log.Printf("adbGetIP regex invalid result: %d", len(list))
+				}
+			}
+		} else {
+			if debugOpt.Error() {
+				log.Printf("adbGetIP regex failed: %s", err.Error())
+			}
+		}
+	}
+	return ""
+}
+
+func adbWirelessConnect(serial, ip string, port int) error {
+	return adbExec(serial, "connect", fmt.Sprintf("%s:%d", ip, port))
+}
+
+func adbWirelessDisconnect(serial, ip string) error {
+	return adbExec(serial, "disconnect", ip)
 }
 
 func adbUSBMod(serial string) error {
@@ -54,7 +119,7 @@ func adbForwardRemove(serial string, localPort int) error {
 		fmt.Sprintf("tcp:%d", localPort))
 }
 
-func adbRun(serial string, params ...string) string {
+/*func adbRun(serial string, params ...string) string {
 	args := make([]string, 0, 8)
 	if len(serial) > 0 {
 		args = append(args, "-s", serial)
@@ -67,36 +132,60 @@ func adbRun(serial string, params ...string) string {
 	}
 
 	cmd := exec.Command(adbCmd, args...)
-	//outinfo := bytes.Buffer{}
-	//cmd.Stdout = &outinfo
-	stdout, _ := cmd.StdoutPipe()
+	outinfo := bytes.Buffer{}
+	cmd.Stdout = &outinfo
+	//stdout, _ := cmd.StdoutPipe()
 
 	err := cmd.Start()
 	if err != nil {
 		if debugOpt.Error() {
-			log.Printf(err.Error())
+			log.Printf("cmd start failed %s", err.Error())
 		}
 		return ""
 	}
 
-	outinfo, _ := ioutil.ReadAll(stdout)
-	stdout.Close()
+	//outinfo, _ := ioutil.ReadAll(stdout)
+	//stdout.Close()
 
 	if err = cmd.Wait(); err != nil {
 		if debugOpt.Error() {
-			log.Printf(err.Error())
+			log.Printf("cmd wait failed %s", err.Error())
 		}
 		return ""
 	}
 	if debugOpt.Debug() {
 		log.Printf("PID %v\n", cmd.ProcessState.Pid())
 		//log.Printf("ExitCode %v\n", cmd.ProcessState.Sys().(syscall.WaitStatus).ExitCode)
-		//log.Printf(outinfo.String())
+		log.Printf(outinfo.String())
 	}
-	return string(outinfo)
+	return outinfo.String()
+}*/
+
+func adbExecOut(serial string, params ...string) (string, error) {
+	var buf bytes.Buffer
+	var berr bytes.Buffer
+	cmd, err := adbExecAsync(&buf, &berr, serial, params...)
+	if err != nil {
+		return "", err
+	}
+	//fmt.Println("adb execute waiting")
+	err = cmd.Wait()
+	if err != nil {
+		return berr.String(), err
+	}
+	return buf.String(), nil
 }
 
-func adbRun1(serial string, params ...string) string {
+func adbExec(serial string, params ...string) error {
+	var buf bytes.Buffer
+	cmd, err := adbExecAsync(&buf, &buf, serial, params...)
+	if err != nil {
+		return err
+	}
+	return cmd.Wait()
+}
+
+func adbExecAsync(pbuf, perr *bytes.Buffer, serial string, params ...string) (*exec.Cmd, error) {
 	args := make([]string, 0, 8)
 	if len(serial) > 0 {
 		args = append(args, "-s", serial)
@@ -107,51 +196,18 @@ func adbRun1(serial string, params ...string) string {
 	if debugOpt.Debug() {
 		log.Printf("执行 %s %s\n", adbCmd, strings.Join(args, " "))
 	}
-
 	cmd := exec.Command(adbCmd, args...)
-	ret, err := cmd.Output()
-	if err != nil {
-		if debugOpt.Error() {
-			log.Printf(err.Error())
-			return ""
-		}
-	}
-	return string(ret)
-}
-
-func adbExec(serial string, params ...string) error {
-	cmd, ret, err := adbExecAsync(serial, params...)
-	if err != nil {
-		return err
-	}
-	err = cmd.Wait()
-	fmt.Println(ret.String())
-	return err
-}
-
-func adbExecAsync(serial string, params ...string) (*exec.Cmd, bytes.Buffer, error) {
-	args := make([]string, 0, 8)
-	if len(serial) > 0 {
-		args = append(args, "-s", serial)
-	}
-	args = append(args, params...)
-
-	adbCmdOnce.Do(getAdbCommand)
-	if debugOpt.Debug() {
-		log.Printf("adbExecAsync %s %s\n", adbCmd, strings.Join(args, " "))
-	}
-	outinfo := bytes.Buffer{}
-	cmd := exec.Command(adbCmd, args...)
-	cmd.Stdout = &outinfo
-	if debugOpt.Debug() {
+	/*if debugOpt.Debug() {
 		cmd.Stderr = os.Stderr
-		cmd.Stdout = os.Stdout
-	}
+		//cmd.Stdout = os.Stdout
+	}*/
+	cmd.Stderr = perr
+	cmd.Stdout = pbuf
 
 	if err := cmd.Start(); err != nil {
-		return nil, outinfo, err
+		return nil, err
 	}
-	return cmd, outinfo, nil
+	return cmd, nil
 }
 
 var adbCmd = "adb"
--- scrcpy/adb_test.go
+++ scrcpy/adb_test.go
@@ -2,18 +2,17 @@ package scrcpy
 
 import (
 	"fmt"
-	"os/exec"
 	"testing"
+	"time"
 )
 
 func Test_exec(t *testing.T) {
-	adbCmdOnce.Do(getAdbCommand)
-	cmd := exec.Command(adbCmd, "devices")
-	ret, err := cmd.Output()
-	if err != nil {
-		t.Errorf(err.Error())
+	ip := adbGetIP("")
+	if len(ip) == 0 {
+		t.Errorf("get ip address failed")
+		return
 	}
-	fmt.Println(string(ret))
+	fmt.Printf("result:\n%s\n", ip)
 }
 
 func Test_adbReverse(t *testing.T) {
@@ -23,8 +22,8 @@ func Test_adbReverse(t *testing.T) {
 	}
 }
 
-func Test_adbTcpMod(t *testing.T) {
-	list := GetDevices()
+func Test_adbWireless(t *testing.T) {
+	list := adbDevices()
 	if len(list) != 1 {
 		t.Errorf("not prepared!")
 		return
@@ -34,7 +33,38 @@ func Test_adbTcpMod(t *testing.T) {
 		t.Error(err.Error())
 		return
 	}
-	list = GetDevices()
+	time.Sleep(time.Duration(2) * time.Second)
+	/*cmd := exec.Command(adbCmd, dvsUSB, "tcpip", fmt.Sprintf("%d", 5555))
+	cmd.Run()*/
+	ip := adbGetIP(dvsUSB)
+	if len(ip) == 0 {
+		t.Errorf("get ip address failed")
+		return
+	}
+	if err := adbWirelessConnect(dvsUSB, ip, 5555); err != nil {
+		t.Error(err.Error())
+		return
+	}
+	list = adbDevices()
+	if len(list) != 2 {
+		t.Errorf("(2)invalid devices %v", list)
+		return
+	}
+	if err := adbWirelessDisconnect(dvsUSB, ip); err != nil {
+		t.Error(err.Error())
+		return
+	}
+	list = adbDevices()
+	if len(list) != 1 {
+		t.Errorf("(1)invalid devices %v", list)
+		return
+	}
+	if err := adbUSBMod(dvsUSB); err != nil {
+		t.Error(err.Error())
+		return
+	}
+	//fmt.Println("IP address: ", ip)
+	/*list = adbDevices()
 	if len(list) != 2 {
 		t.Errorf("invalid devices %v", list)
 		return
@@ -43,9 +73,9 @@ func Test_adbTcpMod(t *testing.T) {
 		t.Error(err.Error())
 		return
 	}
-	list = GetDevices()
+	list = adbDevices()
 	if len(list) != 1 {
 		t.Errorf("invalid devices %v", list)
 		return
-	}
+	}*/
 }
